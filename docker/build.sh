147380403ad2fb8cb69eb8b3e3d3b64e0258d5f8
Jinyoung Park <jyounggo@gmail.com>
2024-08-07 16:45:03 +0000

    Fix uwsgi crash on startup

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,8 @@
-docker build -t csr.csel.io/inginious/inginious:v0.8 .
+#!/bin/bash
+VERSION=v0.8
+NO_CACHE=1
+if [ $NO_CACHE -eq 1 ]; then
+   docker build --no-cache -t csr.csel.io/inginious/inginious:${VERSION} .
+else
+   docker build -t csr.csel.io/inginious/inginious:${VERSION} .
+fi
